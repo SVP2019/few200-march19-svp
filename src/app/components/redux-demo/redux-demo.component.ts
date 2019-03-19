a02358d20856718d5d71ac85c1c58e1f623f2174
SvetaPanin <spanin.sp@gmail.com>
2019-03-19 16:08:08 -0400

    Tuesday afternoon changes

--- src/app/components/redux-demo/redux-demo.component.ts
+++ src/app/components/redux-demo/redux-demo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { State } from '../../reducers';
+import { State, selectCurrent, selectAtStart, selectCountingBy, selectAtChange } from '../../reducers';
 import { Observable } from 'rxjs';
+import * as actions from '../../actions/counter.action';
 
 @Component({
   selector: 'app-redux-demo',
@@ -10,22 +11,32 @@ import { Observable } from 'rxjs';
 })
 export class ReduxDemoComponent implements OnInit {
   count$: Observable<number>;
+  atStart$: Observable<boolean>;
+  countingBy$: Observable<number>;
+  atChange$: Observable<boolean>;
   constructor(private store: Store<State>) { }
 
   ngOnInit() {
     // doing it this way has cost S2 TONS of refactoring time
-    this.count$ = this.store.select(s => s.counter.count);
+    // this.count$ = this.store.select(s => s.counter.count);
+    this.count$ = this.store.select(selectCurrent);
+    this.atStart$ = this.store.select(selectAtStart);
+    this.atChange$ = this.store.select(selectAtChange);
+    this.countingBy$ = this.store.select(selectCountingBy);
   }
 
   increment() {
     // this.count = this.count + 1;
-    this.store.dispatch({ type: 'Increment' });
+    this.store.dispatch(new actions.CountIncremented());
   }
   decrement() {
     // this.count = this.count - 1;
-    this.store.dispatch({ type: 'Decrement' });
+    this.store.dispatch(new actions.CountDecremented());
   }
-  resetCount() {
-    this.store.dispatch({ type: 'Reset' });
+  reset() {
+    this.store.dispatch(new actions.ResetCounter());
+  }
+  setCountBy(val: number) {
+    this.store.dispatch(new actions.SetCountBy(val));
   }
 }
