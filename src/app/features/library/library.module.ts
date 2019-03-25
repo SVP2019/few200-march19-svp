4d711453a2e4220d610458e6f7c533921f25e159
SvetaPanin <spanin.sp@gmail.com>
2019-03-25 15:46:38 -0400

    Added validation

--- src/app/features/library/library.module.ts
+++ src/app/features/library/library.module.ts
@@ -6,12 +6,14 @@ import { EntryComponent } from './components/entry/entry.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers';
 import { LoanComponent } from './components/loan/loan.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LibraryComponent, ListComponent, EntryComponent, LoanComponent],
   imports: [
     CommonModule,
-    StoreModule.forFeature('booksFeature', reducers)
+    StoreModule.forFeature('booksFeature', reducers),
+    FormsModule
   ],
   exports: [
     LibraryComponent
